36b69c41b2e6066883621b24972b43af3848b26f
jaieshjaiswal <jaieshjaiswal7@gmail.com>
2026-05-16 12:31:43 +0530

    fixed final backend url

--- backend/mongoDB.py
+++ backend/mongoDB.py
@@ -1,6 +1,12 @@
 from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
 
-client = MongoClient("mongodb://localhost:27017/")
+load_dotenv()
+
+MONGO_URL = os.getenv("MONGO_URL")
+
+client = MongoClient(MONGO_URL)
 
 db = client["ai_assistant"]
 
